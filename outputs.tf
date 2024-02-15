650ec23a00a5669e3e70086e8df887fb187bc472
Michael Scribellito <mscribellito@gmail.com>
2024-02-15 17:19:28 -0500

    add names output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "names" {
+  value       = [for k in google_secret_manager_secret.secrets : k.secret_id]
+  description = "List of secret names"
+}
+
 output "secrets" {
   value = { for k, v in google_secret_manager_secret.secrets : k =>
     {
